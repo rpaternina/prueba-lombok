a600719501f8f17e74607126d5274695a1883b6c
The Harry Code <theharrycode@gmail.com>
2024-12-10 15:19:08 -0500

    Update PruebaServiceImpl.java

--- src/main/java/com/lombok/service/impl/PruebaServiceImpl.java
+++ src/main/java/com/lombok/service/impl/PruebaServiceImpl.java
@@ -11,25 +11,35 @@ import java.time.LocalDateTime;
 @Service
 public class PruebaServiceImpl implements PruebaService {
 
-    @Autowired
-    public PruebaServiceImpl(PruebaRepository pruebaRepository) {
+	//Puedes hacerlo de 2 maneras.
+	/*Inicio Opcion_1
+     @Autowired PruebaRepository pruebaRepository;
+	 Fin Opcion_ */
 
-    }
+	//La segunda es así:
+	//Inicio Opcion_
+	private final PruebaRepository pruebaRepository;
 
-    @Override
-    public PruebaModel savePrueba(PruebaModel pruebaModel){
+	@Autowired
+	public PruebaServiceImpl(PruebaRepository pruebaRepository) {
+		this.pruebaRepository = pruebaRepository;
+	}
+	//Fin Opcion_2
 
-        if(pruebaModel == null){
-            throw new IllegalArgumentException("PruebaModel no puede ser null");
-        }
-        try {
-            pruebaModel.setFechaRegistro(LocalDateTime.now());
-            return pruebaRepository.save(pruebaModel);
-        } catch (Exception e) {
-            System.err.println("Error en el registro");
-            throw new RuntimeException(e);
-        }
+	@Override
+	public PruebaModel savePrueba(PruebaModel pruebaModel){
 
-    }
+		if(pruebaModel == null){
+			throw new IllegalArgumentException("PruebaModel no puede ser null");
+		}
+		try {
+			pruebaModel.setFechaRegistro(LocalDateTime.now());
+			return pruebaRepository.save(pruebaModel);
+		} catch (Exception e) {
+			System.err.println("Error en el registro");
+			throw new RuntimeException(e);
+		}
+
+	}
 
 }
